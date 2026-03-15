9d89b95034ca6d43493404eeb052c6cdb6e85d12
Maxmomaric029 <112379738+Maxmomaric029@users.noreply.github.com>
2026-03-14 23:15:26 -0400

    Remove static variable definitions in MemoryUtils
    Removed static variable definitions for targetPid and libBase.

--- app/src/MemoryUtils.cpp
+++ app/src/MemoryUtils.cpp
@@ -1,5 +1,2 @@
 #include "MemoryUtils.h"
-
-// Definiciones únicas de las variables estáticas
-pid_t     MemoryUtils::targetPid = 0;
-uintptr_t MemoryUtils::libBase   = 0;
+uintptr_t MemoryUtils::libBase = 0;
